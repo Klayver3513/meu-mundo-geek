5e016c6db6763c7a54e737ba9b4d6eadf542d8bb
Klayver3513 <143143591+Klayver3513@users.noreply.github.com>
2024-06-24 18:47:20 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ app.js
@@ -0,0 +1,65 @@
+let listaDeNumerosSorteados = [];
+let numeroLimite= 10 ;
+let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
+
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+    resposiveVoice.speak(texto,'Brazilian Portuguese Female',{rate:1.2});
+}
+function exibirMensagemInicial(){
+    exibirTextoNaTela('h1', 'jogo do num secreto');
+    exibirTextoNaTela('p', 'escolha o numero entre 1 e 10');
+}
+exibirMensagemInicial();
+
+function verificarChute() {
+    let chute = document.querySelector('input').value;
+
+    if (chute == numeroSecreto) {
+        exibirTextoNaTela('h1', 'Parabens!! ');
+
+        let palavraTentaivas = tentativas > 1 ? 'tentativas' : 'tentativa';
+        let mensagemTentaivas = `voce acertou o numero secreto com ${tentativas} ${palavraTentaivas}!!! `;
+
+        exibirTextoNaTela('p', mensagemTentaivas);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    } 
+    else {
+        if (chute > numeroSecreto) {
+            exibirTextoNaTela('p', 'o numero é menor');
+        } 
+        else {
+            exibirTextoNaTela('p', 'o numero é maior');
+        }
+        tentativas++;
+        limparCampo();
+    }
+}
+
+function limparCampo(){
+    chute=document.querySelector('input');
+    chute.value='';
+}
+
+function gerarNumeroAleatorio() {
+    let numeroEscolhido= parseInt(Math.random() * numeroLimite + 1);
+    let qtdNumerosEscolhidos=listaDeNumerosSorteados.length;
+    if(qtdNumerosEscolhidos== numeroLimite){
+        listaDeNumerosSorteados=[];
+    }
+    if(listaDeNumerosSorteados.includes(numeroEscolhido)){
+        return gerarNumeroAleatorio();
+    }else{
+        listaDeNumerosSorteados.push(numeroEscolhido);
+        return numeroEscolhido;
+    }
+}
+function reiniciarJogo(){
+    numeroSecreto=gerarNumeroAleatorio();
+    limparCampo();
+    tentativas=1;
+    exibirMensagemInicial();
+    document.getElementById('reiniciar').setAttribute('desabled',true)
+}
---
+++ imc/imc.js
@@ -0,0 +1,9 @@
+function imc(altura,peso){
+    let calcular=(altura*altura)/peso;
+}
+function tabuada(numero){
+for(let i=1;1 <=10; i++){
+let resultado= tabuada*i;
+    
+}
+}
